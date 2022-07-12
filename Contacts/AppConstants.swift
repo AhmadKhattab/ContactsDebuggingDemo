5b4e4db5935cfa715c2f3439113f6bd2e0d83150
Ahmad Ashraf Khattab <ahmed.ashrafkhattab@vodafone.com>
2022-07-12 15:44:23 +0200

    add some constants related to lookup contacts screen

--- Contacts/AppConstants.swift
+++ Contacts/AppConstants.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+// MARK: - Lookup Contacts Screen
+let searchBarPlaceholder: String = "Search"
+let contactsTitle: String = "Contacts"
+let unknownContact: String = "Unknown"
+let contactCellReusableIdentifier: String = "contactCell"
+let topNameMatches: String = "Top Name Matches"
+
