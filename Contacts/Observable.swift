7f03b13e389159a784833dfff331e399255f2e7f
Ahmad Ashraf Khattab <ahmed.ashrafkhattab@vodafone.com>
2022-07-14 13:46:37 +0200

    feat: create generic observable

--- Contacts/Observable.swift
+++ Contacts/Observable.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class Observable<T> {
+    typealias Observer = (T) -> Void
+    
+    private var observers: [Observer]
+    var value: T? {
+        didSet {
+            guard let value = value else {
+                return
+            }
+            notifyObservers(value: value)
+        }
+    }
+    
+    init(value: T) {
+        self.value = value
+        self.observers = []
+    }
+    
+    private func notifyObservers(value: T) {
+        observers.forEach { observer in
+            observer(value)
+        }
+    }
+    
+    func bind(observer: @escaping Observer) {
+        observers.append(observer)
+        guard let value = value else {
+            return
+        }
+        notifyObservers(value: value)
+    }
+}
+
