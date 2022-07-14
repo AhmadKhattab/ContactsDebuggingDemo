5fe788345151bab5b594a31e92d40fc6946a7fa8
Ahmad Ashraf Khattab <ahmed.ashrafkhattab@vodafone.com>
2022-07-14 13:52:13 +0200

    feat: rename 'LookUpContactsViewModel' to 'LookupContactsViewModel'

--- Contacts/LookupContactsViewModel.swift
+++ Contacts/LookupContactsViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  LookUpContacsViewModel.swift
+//  LookUpContactsViewModel.swift
 //  Contacts
 //
 //  Created by Ahmad Ashraf Khattab on 13/07/2022.
@@ -8,8 +8,8 @@
 import Foundation
 import UIKit
 
-class LookUpContactsViewModel {
-    
+class LookupContactsViewModel {
+    // MARK: - Properties
     private var contacts: [String] = ["Ahmad Khattab",
                               "Ahmad Elfarsisy",
                               "Ahmad Amin",
@@ -42,29 +42,28 @@ class LookUpContactsViewModel {
                               "Xodia",
                               "Younis Fady",
                               "Zoher Ramy"]
-    
     private var contactsDic: [String: [String]] = [:]
     private var sectionTiles: [String] = []
-    
     private(set) var contactsObservable: Observable<[String]> = Observable(value: [])
     private(set) var contactsDicObservable: Observable<[String: [String]]> = Observable(value: [:])
     private(set) var sectionTilesObservable: Observable<[String]> = Observable(value: [])
     private(set) var filteredContactsDicObservable: Observable<[String: [String]]> = Observable(value: [:])
-    
-    
+    // MARK: - Initializer(s)
     init() {
         setupContactsData()
         contactsDicObservable.value = contactsDic
         sectionTilesObservable.value = sectionTiles
     }
-    
+    // MARK: - Method(s)
     private func setupContactsData() {
         let firstLetter: Int = 1
         sectionTiles = Array(Set(contacts.compactMap{ String($0.prefix(firstLetter)) })).sorted()
         sectionTiles.forEach { contactsDic[$0] = [] }
         contacts.forEach { contactsDic[String($0.prefix(firstLetter))]?.append($0) }
     }
-    
+}
+// MARK: - Handling contacts lookup
+extension LookupContactsViewModel {
     func filterContentForSearchText(_ searchText: String) {
         let filteredContacts = contacts.filter({ contact in
             contact.lowercased().contains(searchText.lowercased())
