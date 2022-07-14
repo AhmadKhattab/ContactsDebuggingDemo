93160dda9c4b0785727cca73aef6f85ad2e7a123
Ahmad Ashraf Khattab <ahmed.ashrafkhattab@vodafone.com>
2022-07-14 13:48:48 +0200

    feat: make ContactsTableViewDiffableDataSource conform UITableViewDiffableDataSource
    Make ContactsTableViewDiffableDataSource to be customized for contacts lookup

--- Contacts/ContactsTableViewDiffableDataSource.swift
+++ Contacts/ContactsTableViewDiffableDataSource.swift
@@ -5,4 +5,45 @@
 //  Created by Ahmad Ashraf Khattab on 13/07/2022.
 //
 
-import Foundation
+import UIKit
+
+class ContactsTableViewDiffableDataSource: UITableViewDiffableDataSource<String, String> {
+    typealias Snapshot = NSDiffableDataSourceSnapshot<String, String>
+    
+    private var contactsDic: [String: [String]] = [:]
+    private var sectionTiles: [String] = []
+    private var filteredContactsDic: [String: [String]] = [:]
+    
+    func updateContactsDic(_ contactsDic: [String: [String]]) {
+        self.contactsDic = contactsDic
+    }
+    
+    func updateSectionTiles(_ sectionTiles: [String]) {
+        self.sectionTiles = sectionTiles
+    }
+    
+    func updateFilteredContactsDic(_ filteredContactsDic: [String: [String]]) {
+        self.filteredContactsDic = filteredContactsDic
+    }
+
+    func applySnapshot(animatingDifferences: Bool = true, isFiltering: Bool = false)
+    {
+        var snapshot = Snapshot()
+        let sections: [String] = isFiltering ? [topNameMatches] : sectionTiles
+        let items: [String: [String]] = isFiltering ? filteredContactsDic : contactsDic
+       
+        snapshot.appendSections(sections)
+        sections.forEach { section in
+            snapshot.appendItems(items[section] ?? [], toSection: section)
+        }
+        self.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionIdentifier(for: section)
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sectionTiles
+    }
+}
